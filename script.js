7780590f96ebc5a3b79163e861a1463d8c384df7
kubal97 <jakubl1997@interia.pl>
2019-04-27 17:51:32 +0200

    Adding navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+/*------Hamburger------*/
+let hamburger = document.getElementById('nav-icon3');
+hamburger.addEventListener('click', function () {
+    console.log("Test");
+    this.classList.toggle('open');
+})
+/*---------------------*/
