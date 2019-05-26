153b52f7d663e28d263cf3d53bcbc406df2b233e
kubal97 <jakubl1997@interia.pl>
2019-05-26 20:39:15 +0200

    Add js filtering functions

--- script.js
+++ script.js
@@ -5,3 +5,45 @@ hamburger.addEventListener('click', function () {
     this.classList.toggle('open');
 })
 /*---------------------*/
+
+/*-------Gallery-------*/
+let images = document.querySelectorAll('.item');
+document.getElementById('all').addEventListener("click", function () {
+    for(let i=0; i<images.length; i++) {
+        images[i].style.display = 'flex';
+    }
+})
+
+document.getElementById('2d').addEventListener("click", function () {
+for(let i=0; i<images.length; i++){
+    if(images[i].classList.contains('2d')){
+        images[i].style.display = 'flex';
+    }
+    else{
+        images[i].style.display = 'none';
+    }
+}
+})
+
+document.getElementById('3d').addEventListener("click", function () {
+    for(let i=0; i<images.length; i++){
+        if(images[i].classList.contains('3d')){
+            images[i].style.display = 'flex';
+        }
+        else{
+            images[i].style.display = 'none';
+        }
+    }
+})
+
+document.getElementById('Frontend').addEventListener("click", function () {
+    for(let i=0; i<images.length; i++){
+        if(images[i].classList.contains('frontend')){
+            images[i].style.display = 'flex';
+        }
+        else{
+            images[i].style.display = 'none';
+        }
+    }
+})
+/*---------------------*/
